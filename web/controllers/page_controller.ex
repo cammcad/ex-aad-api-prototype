a5b8046bbcf73577da03a959dfc2a79556438913
cammcad <cameron.frederick@gmail.com>
2015-04-09 23:42:28 -0700

    Update page_controller.ex

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,12 +4,12 @@ defmodule InterlineAPI.PageController do
   plug :action
 
   def index(conn, _params) do
-		IO.inspect get_req_header(conn,:authorization)
-		IO.inspect get_req_header(conn, :foo)
+    IO.inspect get_req_header(conn,:authorization)
+    IO.inspect get_req_header(conn, :foo)
     render conn, "index.html"
   end
 
-	def api(conn,_params) do
-		send_resp(conn,200,"Hello Interline!")
-	end
+  def api(conn,_params) do
+    send_resp(conn,200,"Hello Interline!")
+  end
 end
